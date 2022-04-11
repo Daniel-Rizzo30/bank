d09605cc93788f8f959f4d2e0fe75b8a8e0a8811
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2022-04-10 20:06:06 -0400

    Added componentDidMount with axios calls to App.js, and got axios to work with the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 
 import React, {Component} from 'react';
+import axios from 'axios';// Library used to send asynchronous HTTP requests to RESTful endpoints (APIs)
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home.js';
 import UserProfile from './components/UserProfile.js';
@@ -16,9 +17,35 @@ class App extends Component {
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '07/23/96',
-      }
+      },
+      credits: [],
+      debits: [],
     }
   }
+
+  // Make async API call to retrieve data from remote website
+  async componentDidMount() {
+    let linkToDebitAPI = 'https://moj-api.herokuapp.com/debits';  // Link to remote website API for Debit
+    let linkToCreditAPI = 'https://moj-api.herokuapp.com/credits';  // Link to remote website API for Credit
+
+    // Await for promise (completion) returned from API call
+    try {  // Accept success response as array of JSON objects (users)
+      let responseDebit = await axios.get(linkToDebitAPI);
+      let responseCredit = await axios.get(linkToCreditAPI);
+      console.log(responseDebit);  // Print out responses
+      console.log(responseCredit); 
+      // To get data object in the response, need to use "response.data"
+      this.setState({credits: responseCredit.data, 
+                     debits: responseDebit.data});  // Store received data in state's object
+    } 
+    catch (error) {  // Print out errors at console when there is an error response
+      if (error.response) {
+        // The request was made, and the server responded with error message and status code.
+        console.log(error.response.data);  // Print out error message (e.g., Not Found)
+        console.log(error.response.status);  // Print out error status code (e.g., 404)
+      }    
+    }
+  }  
 
   // Update state's currentUser (userName) after "Log In" button is clicked
   mockLogIn = (logInInfo) => {  
